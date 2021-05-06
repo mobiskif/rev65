5bf8ac59984606b04f1a2cfd9c7f9cd96c1ce63c
john <mobiskif@hotmail.com>
2021-05-06 08:37:51 +0300

    06/05/21

--- app/src/main/java/ru/rev65/MainActivity.kt
+++ app/src/main/java/ru/rev65/MainActivity.kt
@@ -1,8 +1,13 @@
 package ru.rev65
 
+import android.Manifest
 import android.app.DatePickerDialog
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.CalendarContract
 import android.util.Log
 import android.widget.CalendarView
 import android.widget.DatePicker
@@ -22,6 +27,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.currentCoroutineContext
 import java.io.BufferedReader
@@ -49,8 +57,17 @@ fun getCPUInfo(model: MainViewModel) {
     }
 }
 
+
 class MainActivity : AppCompatActivity() {
     private val model: MainViewModel by viewModels()
+
+    private fun checkPermission(callbackId: Int, vararg permissionsId: String) {
+        var permissions = true
+        for (p in permissionsId) {
+            permissions = permissions && ContextCompat.checkSelfPermission(this, p) == PERMISSION_GRANTED
+        }
+        if (!permissions) ActivityCompat.requestPermissions(this, permissionsId, callbackId)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -102,6 +119,52 @@ class MainActivity : AppCompatActivity() {
             }, year, month, day
         )
         model.dialog = datePickerDialog
+
+        fun dosom(){
+            val EVENT_PROJECTION: Array<String> = arrayOf(
+                CalendarContract.Calendars._ID,                     // 0
+                CalendarContract.Calendars.ACCOUNT_NAME,            // 1
+                CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,   // 2
+                CalendarContract.Calendars.OWNER_ACCOUNT            // 3
+            )
+            val uri: Uri = CalendarContract.Calendars.CONTENT_URI
+            val cur: Cursor = contentResolver.query(uri, EVENT_PROJECTION, null, null, null)!!
+            while (cur.moveToNext()) {
+                // Get the field values
+                val calID: Long = cur.getLong(0)
+                val displayName: String = cur.getString(1)
+                val accountName: String = cur.getString(2)
+                val ownerName: String = cur.getString(3)
+                // Do something with the values...
+                Log.d("jop", "$calID $accountName")
+            }
+
+            val calID: Long = 5
+            val startMillis: Long = Calendar.getInstance().run {
+                set(2021, 4, 4, 17, 30)
+                timeInMillis
+            }
+            val endMillis: Long = Calendar.getInstance().run {
+                set(2021, 4, 4, 18, 45)
+                timeInMillis
+            }
+
+            val values = ContentValues().apply {
+                put(CalendarContract.Events.DTSTART, startMillis)
+                put(CalendarContract.Events.DTEND, endMillis)
+                put(CalendarContract.Events.TITLE, "Title Jopa")
+                put(CalendarContract.Events.DESCRIPTION, "Descrioption Jopa")
+                put(CalendarContract.Events.CALENDAR_ID, calID)
+                put(CalendarContract.Events.EVENT_TIMEZONE, "Europe/Moscow")
+            }
+            val uri2: Uri = contentResolver.insert(CalendarContract.Events.CONTENT_URI, values)!!
+            val eventID: Long = uri2.lastPathSegment!!.toLong()
+            Log.d("jop", uri2.encodedPath + " " + eventID)
+
+        }
+
+        //checkPermission(42, Manifest.permission.READ_CALENDAR, Manifest.permission.WRITE_CALENDAR);
+        //dosom()
     }
 
     override fun onResume() {
